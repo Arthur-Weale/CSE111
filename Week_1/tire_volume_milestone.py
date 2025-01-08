2778b8a278381f944b1c8543435d4c68396e0b45
Arthur Weale <ajayrossy340@gmail.com>
2025-01-08 20:40:22 +0200

    Assignment complete

--- Week_1/tire_volume_milestone.py
+++ Week_1/tire_volume_milestone.py
@@ -1,9 +1,22 @@
 """
 ==========================================
-    Program: Discount
+    Program: Tire Volume
     Author: Arthur Weale
     Date: 1-08-2025
     Description:
         A brief description of what the script does.
 ==========================================
 """
+#Imports the math function
+import math
+
+#Declared Variables 
+tire_width = int(input("Enter the width of the tire in mm: "))
+aspect_ratio = int(input("Enter the aspect ratio of the tire: "))
+tire_diameter = int(input("Enter the diameter os the wheel in inches: "))
+pi = math.pi
+
+#Calculates tire volume
+tire_volume = ((pi * tire_width ** 2 * aspect_ratio) * (tire_width * aspect_ratio + 2540 * tire_diameter))/10000000000
+print(f'The approximate volume is {tire_volume:.2f}')
+
