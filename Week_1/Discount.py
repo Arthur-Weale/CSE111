5fb17bfd14d4c62d54942d6c57bf68f242c01a51
Arthur Weale <ajayrossy340@gmail.com>
2025-01-08 14:34:40 +0200

    Add date functions

---
+++ Week_1/Discount.py
@@ -0,0 +1,18 @@
+"""
+==========================================
+    Program: Discount
+    Author: Arthur Weale
+    Date: 1-08-2025
+    Description:
+        A brief description of what the script does.
+==========================================
+"""
+
+# Import required modules
+from datetime import datetime  
+
+today = datetime.now()
+current_day = today.strftime("%A")
+print(current_day)
+
+
