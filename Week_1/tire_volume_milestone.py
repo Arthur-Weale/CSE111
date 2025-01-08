ea44e5626acf95670ef26fabdbb56dda9f01932c
Arthur Weale <ajayrossy340@gmail.com>
2025-01-08 16:45:28 +0200

    Add tire volume milestone python project file

---
+++ Week_1/tire_volume_milestone.py
@@ -0,0 +1,9 @@
+"""
+==========================================
+    Program: Discount
+    Author: Arthur Weale
+    Date: 1-08-2025
+    Description:
+        A brief description of what the script does.
+==========================================
+"""
