8ecd539cc2c73a7d02860c1ea88b76d2420b22e7
Arthur Weale <ajayrossy340@gmail.com>
2025-01-08 21:46:06 +0200

    Add text file

--- Week_1/tire_volume.py
+++ Week_1/tire_volume.py
@@ -19,9 +19,13 @@ aspect_ratio = int(input("Enter the aspect ratio of the tire: "))
 tire_diameter = int(input("Enter the diameter os the wheel in inches: "))
 pi = math.pi
 today = datetime.now()
-
+print(today)
 #Calculates tire volume
 def calculate_tire_volume ():
     tire_volume = ((pi * tire_width ** 2 * aspect_ratio) * (tire_width * aspect_ratio + 2540 * tire_diameter))/10000000000
     print(f'The approximate volume is {tire_volume:.2f}')
     return tire_volume
+
+calculate_tire_volume()
+
+#Writing in text file
