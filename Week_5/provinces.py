59b5612fd487c7e25b457f6bebcf52d2cc589d7f
Arthur Weale <ajayrossy340@gmail.com>
2025-02-04 20:48:54 +0200

    Add python and txt file

---
+++ Week_5/provinces.py
@@ -0,0 +1,14 @@
+import csv
+
+
+def main():
+    province_names = get_provinces(provinces.txt)
+
+def get_provinces(filename):
+
+    with open(filename, "rt") as province_file
+
+
+
+if __name__ == "__main__":
+    main()
